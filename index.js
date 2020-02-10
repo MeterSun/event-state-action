f3f3cc8375a185b76d37f74041910ddfdf6f2194
metersun <zydyouxiang@hotmail.com>
2020-02-10 16:53:04 +0800

    完善发布订阅的流程

--- index.js
+++ index.js
@@ -6,26 +6,51 @@ const PathCompare = require("./lib/PathCompare");
 class ESA {
     constructor(props) {
         this.root = new ObjTree();
+        this.publishList = [];
+        this.isRunningSubscribeAction = false;
     }
-    init(stateName, initialstate) {
+    init(stateName, initialstate = undefined) {
         const name = new EventStateName(stateName);
-        this.root.addChild(name.stateName, new State(initialstate));
+        const newNode = new State(initialstate)
+        const withTag = (...tags) => newNode.addTag(...tags);
+        this.root.addChild(name.stateName, newNode);
+        return { withTag };
+    }
+    _publish(eventName, param) {
+        const name = new EventStateName(eventName);
+        const esa = this;
+        this.root.recursionChildFirst(function () {
+            if (name.tagName === '[@all]' && PathCompare.ArootBchild(this.path, name.path)) {
+                this.root.exec(name.tagName, param)
+            }
+            if (PathCompare.equal(this.path, name.path)) {
+                this.root.exec(name.tagName, param);
+                esa.isRunningSubscribeAction = false;
+                // } else if (PathCompare.ArootBchild(this.path, name.path)) {
+                //     this.root.exec(param);
+            }
+            esa.runPublishList();
+        });
     }
     publish(eventName, param) {
         const name = new EventStateName(eventName);
-        this.root.recursionChildFirst(function () {
-            if (PathCompare.equal(this.path, name.path)) {
-                this.root.exec(name.tagName, param);
-            // } else if (PathCompare.ArootBchild(this.path, name.path)) {
-            //     this.root.exec(param);
-            }
-        });
+        if (name.stateName && !this.root.findChildNode(name.stateName)) throw new Error(`State '${name.stateName}' is not initialized`);
+        this.publishList.push(() => this._publish(eventName, param));
+        if (!this.isRunningSubscribeAction) this.runPublishList();
+    }
+    runPublishList() {
+        if (this.publishList.length > 0 && !this.isRunningSubscribeAction) {
+            let [fun] = this.publishList.splice(0, 1);
+            this.isRunningSubscribeAction = true;
+            fun()
+        }
     }
     subscribe(eventName, callback) {
         const name = new EventStateName(eventName);
+        if (name.stateName && !this.root.findChildNode(name.stateName)) throw new Error(`State '${name.stateName}' is not initialized`);
         this.root.recursionChildFirst(function () {
-            if (PathCompare.equal(this.path, name.path)) { 
-                this.root.addAction(name.tagName,callback);
+            if (PathCompare.equal(this.path, name.path)) {
+                this.root.addAction(name.tagName, callback);
             }
         });
     }
@@ -35,6 +60,18 @@ class ESA {
             if (stateName === this.path.join(".")) result = this.root.valueOf();
         });
         return result;
+    }
+    /**
+     * alias name of publish
+     */
+    pub(...params) {
+        this.publish(...params)
+    }
+    /**
+     * alias name of subscribe
+     */
+    sub(...params) {
+        this.subscribe(...params)
     }
 }
 
